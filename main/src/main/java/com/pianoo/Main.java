9cf82e27f4338644f0331f9f8f913e17d3f55d42
agathemczk <agathe.maciaszczyk@icloud.com>
2025-02-10 10:34:15 +0100

    first commit

---
+++ main/src/main/java/com/pianoo/Main.java
@@ -0,0 +1,7 @@
+package com.pianoo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ share/src/main/java/com/pianoo/view/IView.java
@@ -0,0 +1,4 @@
+package com.pianoo.view;
+
+public interface IView {
+}
